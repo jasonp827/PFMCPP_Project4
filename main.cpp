a1343c8710f139aef5f5c8e6ff9521b264eb4d07
jasonp827 <jasonp827@gmail.com>
2020-06-07 19:24:50 +0000

    Finished Project 4 Part1

--- main.cpp
+++ main.cpp
@@ -51,6 +51,101 @@
  */
 
 #include <iostream>
+
+struct FloatType
+{
+    float add( float lhs, float rhs );
+    float subtract( float lhs, float rhs );
+    float multiply( float lhs, float rhs );
+    float divide( float lhs, float rhs );
+};
+
+float FloatType::add(float lhs, float rhs)
+{
+    return lhs + rhs;
+}
+
+float FloatType::subtract(float lhs, float rhs)
+{
+    return lhs - rhs;
+}
+
+float FloatType:: multiply(float lhs, float rhs)
+{
+    return lhs * rhs;
+}
+float FloatType::divide(float lhs, float rhs)
+{
+    if(rhs == 0.0f)
+    {
+        std::cout << "Warning! You are about to divide by float 0" << "\n";
+    }
+    return lhs / rhs;
+}
+
+struct DoubleType
+{
+    double add( double lhs, double rhs );
+    double subtract( double lhs, double rhs );
+    double multiply( double lhs, double rhs );
+    double divide( double lhs, double rhs );
+};
+
+double DoubleType::add(double lhs, double rhs)
+{
+    return lhs + rhs;
+}
+
+double DoubleType::subtract(double lhs, double rhs)
+{
+    return lhs - rhs;
+}
+
+double DoubleType:: multiply(double lhs, double rhs)
+{
+    return lhs * rhs;
+}
+double DoubleType::divide(double lhs, double rhs)
+{
+    if(rhs == 0.0)
+    {
+        std::cout << "Warning! You are about to divide by double 0" << "\n";
+    }
+    return lhs / rhs;
+}
+
+struct IntType
+{
+    int add( int lhs, int rhs );
+    int subtract( int lhs, int rhs );
+    int multiply( int lhs, int rhs );
+    int divide( int lhs, int rhs );
+};
+
+int IntType::add(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+int IntType::subtract(int lhs, int rhs)
+{
+    return lhs - rhs;
+}
+
+int IntType:: multiply(int lhs, int rhs)
+{
+    return lhs * rhs;
+}
+int IntType::divide(int lhs, int rhs)
+{
+    if(rhs == 0)
+    {
+        std::cout << "Warning! You are about to divide by int 0. Cancelling operation by returning 0." << "\n";
+        return 0;
+    }
+    return lhs / rhs;
+}
+
 int main() 
 {
     FloatType ft;
